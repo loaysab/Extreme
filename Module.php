99f5f82ca5cb9fe1f744fe03ca69edf4180b8104
loaysab <64496809+loaysab@users.noreply.github.com>
2020-04-28 22:44:14 +0300

    Add files via upload

---
+++ Module.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Electus;
+
+
+class Module
+{
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Expected to return \Zend\ServiceManager\Config object or array to
+     * seed such an object.
+     *
+     * @return array|\Zend\ServiceManager\Config
+     */
+    public function getServiceConfig()
+    {
+        return [];
+    }
+
+}
